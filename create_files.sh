b7f92818555d51ebf53980ead0bc059404ccac95
Elena Tembeleski <elenatembeleski@Elenas-MacBook-Air-3.local>
2025-12-17 10:23:35 +1100

    ../create_files.sh

---
+++ create_files.sh
@@ -0,0 +1,15 @@
+ 
+
+#!/bin/bash 
+
+while IFS= read -r line; do 
+
+    [ -z "$line" ] && continue 
+
+    echo "Creating file: $line" 
+
+    mkdir -p "$(dirname "$line")" 
+
+    touch "$line" 
+
+done < files.txt 
